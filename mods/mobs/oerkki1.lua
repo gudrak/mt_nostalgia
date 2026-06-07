4588b18b104cbbc65233c35670626058591b500b
gudrak <63885769+gudrak@users.noreply.github.com>
2026-06-07 18:09:02 -0300

    Rename mob 'oerkki1' with spawn settings

---
+++ mods/mobs/oerkki1.lua
@@ -0,0 +1,38 @@
+mobs:register_mob("mobs:oerkki1", {
+    type = "monster",
+    hp_max = 8,
+    collisionbox = {-0.4, -1, -0.4, 0.4, 1, 0.4},
+    stepheight = 1.1,
+    visual = "upright_sprite",
+    textures = {"oerkki1_front.png", "oerkki1_back.png"},
+    damaged_textures = {"oerkki1_damaged_front.png", "oerkki1_damaged_back.png"},
+    visual_size = {x=2, y=2},
+    makes_footstep_sound = false,
+    view_range = 15,
+    walk_velocity = 1,
+    run_velocity = 3,
+    damage = 4,
+    drops = {},
+    armor = 100,
+    drawtype = "front",
+    light_resistant = true,
+    water_damage = 1,
+    lava_damage = 1,
+    light_damage = 0,
+    attack_type = "dogfight",
+    animation = {
+        stand_start = 0,
+        stand_end = 23,
+        walk_start = 24,
+        walk_end = 36,
+        run_start = 37,
+        run_end = 49,
+        punch_start = 37,
+        punch_end = 49,
+        speed_normal = 15,
+        speed_run = 15,
+    },
+})
+
+--mobs:register_spawn("mobs:oerkki1", {"default:stone"}, 2, -1, 7000, 3, -10)
+mobs:register_spawn("mobs:oerkki1", {"default:stone"}, 2, -1, -20, 3, -10)
