e64982f85d3f0ae1ac629b17b9b050faa9099141
gudrak <63885769+gudrak@users.noreply.github.com>
2026-06-07 19:17:28 -0300

    Actualizar init.lua

--- mods/mobs/init.lua
+++ mods/mobs/init.lua
@@ -1,5 +1,5 @@
 dofile(minetest.get_modpath("mobs").."/api.lua")
-dofile(minetest.get_modpath("mobs").."/oerkki.lua")
+dofile(minetest.get_modpath("mobs").."/oerkki1.lua")
 dofile(minetest.get_modpath("mobs").."/rat.lua")
 dofile(minetest.get_modpath("mobs").."/dm.lua")
 
